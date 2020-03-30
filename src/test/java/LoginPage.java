b3ca123b6613908180a8b66e93bcabfceb508c09
tomskih <tomskih.e@gmail.com>
2020-03-31 00:50:54 +0500

    solition

--- src/test/java/LoginPage.java
+++ src/test/java/LoginPage.java
@@ -1,2 +1,35 @@
+import com.codeborne.selenide.Selenide;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.page;
+
 public class LoginPage {
+    private WebDriver driver;
+    private By imgLogin = By.className("V64Sp");
+    public String login;
+    public String pass;
+
+    public By loginInput = By.name("username");
+    public By passInput = By.name("password");
+    private By loginButton = By.xpath("//button[@type='submit']");
+
+    public LoginPage open() {
+        Selenide.open("/");
+        return this;
+    }
+
+    public void setLogin(String login){
+        driver.findElement(loginInput).sendKeys(login);
+    }
+
+    public void setPassword(String pass){
+        driver.findElement(passInput).sendKeys(pass);
+    }
+
+    public void clickLoginButton(){
+        driver.findElement(loginButton).click();
+
+    }
 }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,10 +1,37 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.open;
 
+
 public class LoginTest {
-    @Test
-    public void openPage(){
-        open("https://www.instagram.com/");
+    @BeforeClass
+    public static void setup() {
+        Configuration.baseUrl = "https://www.instagram.com";
     }
+
+    @Test
+    public void submitLoginForm() throws InterruptedException {
+        LoginPage loginPage = new LoginPage();
+        loginPage.open();
+        loginPage.setLogin("123456789");
+
+        loginPage.wait(10000);
+        loginPage.setPassword("58865");
+
+    }
+ /*   public void userCanSearch() {
+        open("https://google.com/ncr");
+        new LoginPage().searchFor("selenide");
+
+        SearchResultsPage results = new SearchResultsPage();
+        results.getResults().shouldHave(sizeGreaterThan(1));
+        results.getResult(0).shouldHave(text("Selenide: concise UI tests in Java"));
+    }
+
+  */
 }
